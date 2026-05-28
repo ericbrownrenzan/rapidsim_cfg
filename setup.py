1b7464e73dfcc9c7c6eeb42c7d3bdb66300ef137
rzan <renzan1994@hotmail.com>
2026-05-28 21:45:19 +0800

    update readme.md

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Setup script for RapidSim_Cfg package."""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="RapidSim_Cfg",
+    version="0.1.0",
+    author="Zan Ren",
+    author_email="zan.ren@cern.ch",
+    description="Generate RapidSim configuration files programmatically",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ericbrownrenzan/rapidsim_cfg",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        # No external dependencies required
+    ],
+)
